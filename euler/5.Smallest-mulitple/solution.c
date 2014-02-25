d89520d00055bed3e7a3eb8b51cd9c54cceba970
Taemin Hong <taemin@lodics.com>
2014-02-25 20:59:39 +0900

    Rewrite C code iteratively.

--- euler/5.Smallest-mulitple/solution.c
+++ euler/5.Smallest-mulitple/solution.c
@@ -1,12 +1,18 @@
 #include <assert.h>
 #include <stdio.h>
 
+int least_common_multiple_range(int first, int last);
+int least_common_multiple(int a, int b);
+int greatest_common_divisor(int a, int b);
+
 int least_common_multiple_range(int first, int last)
 {
         assert(first <= last);
-        if (first == last)
-                return first;
-        return least_common_multiple(first, least_common_multiple_range(first+1, last));
+        int lcm = first;
+        for (++first; first<=last; ++first) {
+                lcm = least_common_multiple(lcm, first);
+        }
+        return lcm;
 }
 
 int least_common_multiple(int a, int b)
