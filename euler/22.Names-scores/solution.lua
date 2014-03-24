da275704a759c24d3ba6636f94a303d639021c91
Taemin Hong <taemin@lodics.com>
2014-03-24 14:42:01 +0900

    Print the sum of name scores directly in the code read from 'name.txt'.

--- euler/22.Names-scores/solution.lua
+++ euler/22.Names-scores/solution.lua
@@ -5,23 +5,25 @@ function  get_alphabetical_order(alpha)
    return string.find("ABCDEFGHIJKLMNOPQRSTUVWXYZ",  alpha:upper())
 end
 
-function calculate_name_score(name)
+function calculate_name_score(name, index)
    local score = 0
    for c in name:gmatch "." do
       score = score + get_alphabetical_order(c)
    end
-   return score
+   return score * index
 end
 
-assert(calculate_name_score("COLIN") == 53)
+function sum_name_scores(names)
+   local sum = 0
+   table.sort(names)
+   for i, v in ipairs(names) do
+      sum = sum + calculate_name_score(v, i)
+   end
+   return sum
+end
+
+assert(calculate_name_score("COLIN", 938) == 49714)
 
 file = io.open("names.txt")
-code = "return {" .. file:read() .. "}"
-names = assert(loadstring(code))()
-name_score_sum = 0
-table.sort(names)
-for i, v in ipairs(names) do
-   name_score_sum = name_score_sum + calculate_name_score(v) * i
-end
-print(name_score_sum)
-
+code = "print(sum_name_scores{" .. file:read() .. "})"
+assert(loadstring(code))()
