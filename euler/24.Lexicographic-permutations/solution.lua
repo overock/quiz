1f573069bffef5cabfe74a82d39f8c2c9951209c
Taemin Hong <taemin@lodics.com>
2014-03-27 15:56:20 +0900

    Solve the problem 24 'Lexicographic permutations'.

---
+++ euler/24.Lexicographic-permutations/solution.lua
@@ -0,0 +1,82 @@
+#!/usr/bin/env lua
+
+function permutations(...)
+   local cache = {}
+   function iter(arr, start)
+      if start == #arr then
+         return {arr[start]}
+      end
+      local result = {}
+      for i = start, #arr do
+         -- swap
+         arr[start], arr[i] = arr[i], arr[start]
+         local sub_perm = iter(arr, start+1)
+         for _, w in ipairs(sub_perm) do
+            table.insert(result, arr[start] .. w)
+         end
+         -- restore
+         arr[start], arr[i] = arr[i], arr[start]      
+      end
+      return result
+   end
+   return iter({...}, 1)
+end
+
+function lexicographic_permutations(...)
+   local p = permutations(...)
+   table.sort(p)
+   return p
+end
+
+function factorial(n)
+   if n == 1 then
+      return 1
+   else
+      return n * factorial(n-1)
+   end
+end
+
+function number_of_permutations(n)
+   return factorial(n)
+end
+
+function last_lexicographic_permutation(arr)
+   table.sort(arr, function (a, b) return b < a end)
+   return table.concat(arr, "")
+end
+
+function nth_lexicographic_permutation(n, ...)
+   local function iterate(n, arr)
+      local upper_bound = number_of_permutations(#arr)
+      local stride = upper_bound / #arr
+      if upper_bound < n then
+         return nil
+      elseif upper_bound == n then
+         return last_lexicographic_permutation(arr)
+      else
+         local r = n % stride
+         if r == 0 then
+            r = stride
+         end
+         local q = (n - r) / stride + 1
+         assert(q <= #arr)
+         assert(q > 0)
+         assert(r > 0)
+         assert(r <= stride)
+         local head = arr[q]
+         table.remove(arr, q)
+         local smaller_permutation = iterate(r, arr)
+         return head .. smaller_permutation
+      end
+   end
+   local arr = {...}
+   table.sort(arr)
+   return iterate(n, arr)
+end
+
+result = lexicographic_permutations("0", "1", "2")
+assert(table.concat(result, " ") == "012 021 102 120 201 210")
+--result = lexicographic_permutations("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+--print(result[1000000])
+print(nth_lexicographic_permutation(1000000, "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"))
+
