45800052e2e41a28b726d2cc10a55aedcc72fd84
Taemin Hong <taemin@lodics.com>
2014-04-22 21:27:12 +0900

    Coin sum: reduce iterator() calls.

--- euler/31.Coin-sums/solution.lua
+++ euler/31.Coin-sums/solution.lua
@@ -9,21 +9,33 @@ function table.reverse(a)
 end
 
 function count_ways_to_make_change(coins, money)
+   local ncalls = 0
+   local nfails = 0
    local function iterate(coins, money, pos, total)
-      if pos > #coins or money < 0 then
-         return 0
-      elseif money == 0 then
+      ncalls = ncalls + 1
+      if money == 0 then
          return 1
+      elseif pos > #coins or money < 0 then
+         nfails = nfails + 1
+         return 0
       else
-         return iterate(coins, money - coins[pos], pos, total + coins[pos]) +
-                iterate(coins, money, pos + 1, total)
+         local sum = 0
+         local r = money % coins[pos]
+         local n = (money - r) / coins[pos]
+         for i = 0, n do
+            local x = coins[pos] * i
+            sum = sum + iterate(coins, money - x, pos+1, total + x)
+         end
+         return sum
       end
    end
-   return iterate(coins, money, 1, 0)
+   return iterate(coins, money, 1, 0), ncalls, nfails
 end
 
---[[ It is more faster to count with decremet orderd coins
-   rather than increment orderd one. ]]--
+--[[ It is more faster to count with descending orderd coins
+   rather than ascending orderd one. ]]--
+print("count", "ncalls", "nfails")
 coins = {1, 2, 5, 10, 20, 50, 100, 200}
+--print(count_ways_to_make_change(coins, 200))
 table.reverse(coins)
 print(count_ways_to_make_change(coins, 200))
