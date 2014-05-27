e82e8fff6e4b69d60a8a9bc88307c156453f8918
Taemin Hong <taemin@lodics.com>
2014-05-27 14:46:46 +0900

    Digit factorials: improve performance.

--- euler/34.Digit-factorials/solution.lua
+++ euler/34.Digit-factorials/solution.lua
@@ -7,13 +7,6 @@ for n = 1, 9 do
    factorial[n] = factorial[n-1] * n
 end
 
-for n = 2, math.huge do
-   max = 10 ^ (n - 1)
-   if factorial[9] * n < max then
-      break
-   end
-end
-
 function factorial_sum(n)
    local sum = 0
    while n ~= 0 do
@@ -26,13 +19,37 @@ end
 
 assert(factorial_sum(145) == 145)
 
--- Find solution
-total = 0
-for n = 3, max do
-   if n == factorial_sum(n) then
-      total = total + n
+
+for n = 2, math.huge do
+   max_exp = n - 1
+   if (factorial[9] * n) < (10 ^ max_exp) then
+      break
    end
 end
 
-print(total)
+function find_solution()
+   function acc(total, n)
+      local x = factorial[9] * (n - 1)
+      for head = 1, 9 do
+         local lower_bound = head * (10 ^ n)
+         local upper_bound = (head + 1) * (10 ^ n) - 1
+         for i = lower_bound, upper_bound do
+            if i > factorial[head] + x then
+               break
+            elseif i == factorial_sum(i) then
+               total = total + i
+            end
+         end
+      end
+      return total
+   end
+
+   total = 0
+   for n = 1, max_exp do
+      total = acc(total, n)
+   end
+   return total
+end
+
+print(find_solution())
 
